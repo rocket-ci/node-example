c5ab5d4bd5418eae905801e8a432957ffb32ea68
ryohey <r.k.blvdr+github@gmail.com>
2016-10-12 17:30:31 +0900

    add files

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const fs = require("fs")
 
-fs.writeFileSync(`${process.env.ROCKET_ARTIFACTS}/test.txt`, "foo bar")
+const dir = process.env.ROCKET_ARTIFACTS
+fs.mkdirSync(`${dir}/hoge`)
+fs.mkdirSync(`${dir}/hoge/fuga`)
+fs.mkdirSync(`${dir}/moga`)
+fs.mkdirSync(`${dir}/hoha`)
+fs.writeFileSync(`${dir}/test.txt`, "foo bar")
+fs.writeFileSync(`${dir}/test2.txt`, "foo bar")
+fs.writeFileSync(`${dir}/hoha/test.txt`, "foo bar")
+fs.writeFileSync(`${dir}/hoge/test.txt`, "foo bar")
+fs.writeFileSync(`${dir}/hoge/test2.txt`, "foo bar")
+fs.writeFileSync(`${dir}/hoge/fuga/test.txt`, "foo bar")
